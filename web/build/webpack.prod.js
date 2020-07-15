67d154d8d0ac437834a982c829a32c095b42cf6e
guoyihao <guoyihao@icasc.cn>
2020-07-15 08:43:02 +0800

    build指令的生成html文件

--- web/build/webpack.prod.js
+++ web/build/webpack.prod.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-01 11:22:59
- * @LastEditTime: 2020-07-14 16:42:09
+ * @LastEditTime: 2020-07-15 08:39:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \web\webpack.prod.js
@@ -14,7 +14,16 @@ const {
 } = require('clean-webpack-plugin');
 const common = require('./webpack.common.js');
 const config = require('../config');
-console.log('====config', config)
+// console.log('====config', config)
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebpackPlugin({
+  // 生成的html的文件名
+  filename: `index.html`,
+  // 引用的模板
+  template: './build/index.html',
+  // 注入bundle到body中
+  inject: true
+});
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
@@ -25,6 +34,6 @@ module.exports = merge(common, {
         to: config.build.assetsSubDirectory
       }]
     })
-  ],
+  ].concat(htmlPlugin),
   devtool: '#source-map',
 });
